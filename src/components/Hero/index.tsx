cd4466ba4b0a3a5310aa2bea6f9755a96c3121ff
Sidney Rodrigues <sidneyr_gyn@hotmail.com>
2025-08-02 22:42:55 -0300

    ajustes

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,7 +1,6 @@
 // import { Link } from 'react-router-dom'
 import { HeroContainer } from './styles'
 import heroBackground from '../../assets/images/vector.png'
-
 import { Logo } from '../Logo'
 
 const Hero = () => (
---
+++ src/pages/Perfil/index.tsx
@@ -0,0 +1,8 @@
+export const Perfil = () => {
+  return (
+    <div>
+      <h1>Perfil</h1>
+      <p>Detalhes do perfil do usuário.</p>
+    </div>
+  )
+}
